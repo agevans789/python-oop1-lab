6b0266fcbcc4e217d45525c362d633796b5ae7a6
Ann Grace Evans <agevans789@gmail.com>
2026-02-18 22:02:05 -0500

    5 passed

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+        @property
+        def size(self):
+            return self._size
+        
+        @size.setter
+        def size(self, value):
+            value = value.lower().strip()
+            sizes = ["small", "medium", "large"]
+            if value not in sizes:
+                raise ValueError(f"size must be one of {sizes}.")
+            self._size = value
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
